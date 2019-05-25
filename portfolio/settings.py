a2f44bbb177f0fad5dbf54e8ad3f4b2fbf2cb1c0
Turza006 <50375763+Turza006@users.noreply.github.com>
2019-05-26 01:56:54 +0600

    add home html file for jobs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
+        'NAME': 'portfoliodbk',
         'USER':'postgres',
         'PASSWORD':'kajolkona',
         'HOST':'localhost',
