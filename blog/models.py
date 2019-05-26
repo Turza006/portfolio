96b7b390d978e0ab2f4757085c6c8eac2ca6e960
Turza006 <50375763+Turza006@users.noreply.github.com>
2019-05-27 00:36:13 +0600

    jobs done

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Blog(models.Model):
     title=models.CharField(max_length=255)
     pub_date=models.DateTimeField()
-    body = models.TextField
+    body = models.TextField(default='')
     image = models.ImageField(upload_to='images/')
