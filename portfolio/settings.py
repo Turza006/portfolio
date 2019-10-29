1e86e94ab073ec706fac0c4801e0fda132f8df57
Turza006 <probaldattaturza@gmail.com>
2019-10-29 20:58:48 +0600

    security update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,14 +79,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodbk',
-        'USER':'postgres',
-        'PASSWORD':'kajolkona',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+
 
 
 # Password validation
